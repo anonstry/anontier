c33dc0260b2f80d28dee2fea63ef79d02dbbc829
Anonstry <anonstry@protonmail.com>
2024-07-25 23:27:46 -0300

    adicionando informações sobre repositório atual

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
-from hydrogram import Client
 from dynaconf import settings
+from hydrogram import Client
+
+# from git.repo import Repo
 # from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 client: Client = Client(
@@ -8,5 +10,16 @@ client: Client = Client(
     api_hash=settings.TELEGRAM_API_HASH,
     bot_token=settings.TELEGRAM_BOT_TOKEN,
 )
+
+# repository = Repo(search_parent_directories=True)
+
+# latest_repository_tag = sorted(
+#     repository.tags, key=lambda tag: tag.commit.committed_datetime
+# )[-1].name
+# latest_repository_commit_shorted = repository.git.rev_parse(
+#     repository.head.commit.hexsha, short=True
+# )
+# print(f"Lastest repository tag is: {latest_repository_tag}")
+# print(f"Lastest repository commit shorted is {latest_repository_commit_shorted}")
 
 # scheduler = AsyncIOScheduler()
