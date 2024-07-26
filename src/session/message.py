fba445ee0fb4dc98f82dd40628ef3bd257f521f7
Anonstry <anonstry@protonmail.com>
2024-07-26 06:44:14 -0300

    adding experimental variables

--- src/session/message.py
+++ src/session/message.py
@@ -1,13 +1,9 @@
-from pymongo import MongoClient
-from dynaconf import settings
+from loguru import logger
 
-import secrets
-
-mongo_client = MongoClient(settings.MONGO_CONNECTION_STRING)
-mongo_database = mongo_client[settings.MONGO_DATABASE_NAME]
+from src.session import mongo_database, create_token
 
 
-class Message:  # RoomMessage
+class DatabaseMessage:
     mongo_collection = mongo_database["messages"]
 
     def __init__(
@@ -19,12 +15,13 @@ class Message:  # RoomMessage
         from_primary_room_token=None,
         from_primary_message_token=None,
     ):
-        self.token = token or secrets.token_hex(32)
+        self.token = token or create_token(48)
         self.from_telegram_chat_id = from_telegram_chat_id
         self.from_room_token = from_room_token
         self.telegram_message_id = telegram_message_id
         self.from_primary_room_token = from_primary_room_token
         self.from_primary_message_token = from_primary_message_token
+        self.expiration_timestamp = "Nothing here yet" # @experimental @sanitization
 
     def exists(self):
         query = {
@@ -71,34 +68,30 @@ class Message:  # RoomMessage
 
 
 def search_linked_messages(primary_message_token):  # maybe include_itself=True
-    mongo_collection = Message.mongo_collection
-
-    linked_messages = mongo_collection.find(
+    mongo_collection = DatabaseMessage.mongo_collection
+    database_linked_messages = mongo_collection.find(
         {"from_primary_message_token": primary_message_token}
     )
-    linked_messages = list(linked_messages)
-    if not linked_messages:
-        print("No linked messages", linked_messages)
-        return list()  # Empty list
+    for database_linked_message in database_linked_messages:
+        database_linked_message = DatabaseMessage(
+            database_linked_message["from_telegram_chat_id"],
+            database_linked_message["from_room_token"],
+            database_linked_message["telegram_message_id"],
+        )
+        database_linked_message.refresh()
+        yield database_linked_message
     else:
-        for linked_message in linked_messages:
-            _linked_message = Message(
-                linked_message["from_telegram_chat_id"],
-                linked_message["from_room_token"],
-                linked_message["telegram_message_id"],
-            )
-            _linked_message.refresh()
-            yield _linked_message
+        logger.error("No linked messages!")
+        yield from list()  # Empty list
 
 
 def search_correspondent_replied_message(
     where_telegram_chat_id,
     where_room_token,
     with_primary_message_token,
-):  # maybe include_itself=True
-    mongo_collection = Message.mongo_collection
-
-    linked_message = mongo_collection.find_one(
+):
+    mongo_collection = DatabaseMessage.mongo_collection
+    correspondent_database_message = mongo_collection.find_one(
         {
             "from_telegram_chat_id": where_telegram_chat_id,
             "from_room_token": where_room_token,
@@ -113,32 +106,35 @@ def search_correspondent_replied_message(
             ],
         }
     )
-    assert linked_message
-    message = Message(
-        from_telegram_chat_id=linked_message["from_telegram_chat_id"],
-        from_room_token=linked_message["from_room_token"],
-        telegram_message_id=linked_message["telegram_message_id"],
-    )
-    message.refresh()
-    return message
+    assert correspondent_database_message
+    # database_message = DatabaseMessage(
+    #     from_telegram_chat_id=correspondent_database_message["from_telegram_chat_id"],
+    #     from_room_token=correspondent_database_message["from_room_token"],
+    #     telegram_message_id=correspondent_database_message["telegram_message_id"],
+    # )
+    # database_message.refresh()
+    # return database_message
+    return correspondent_database_message
 
 
-def search_for_deleted_message_with_id(telegram_message_id):  # session.telegram
-    mongo_collection = Message.mongo_collection
-
-    message_document = mongo_collection.find_one(
+def search_for_original_messages_with_id(telegram_message_id):
+    mongo_collection = DatabaseMessage.mongo_collection
+    database_messages_documents = mongo_collection.find(
         {
             "telegram_message_id": telegram_message_id,
             "from_primary_room_token": None,
-            "$expr": {"$ne": ["$from_room_token", None]},
+            "$expr": {"$ne": ["$from_room_token", None]},  # is not Null
             "from_primary_message_token": None,
         }
     )
-    assert message_document
-    _message = Message(
-        from_telegram_chat_id=message_document["from_telegram_chat_id"],
-        from_room_token=message_document["from_room_token"],
-        telegram_message_id=message_document["telegram_message_id"],
-    )
-    _message.refresh()
-    return _message
+    for database_message_document in database_messages_documents:
+        database_message = DatabaseMessage(
+            from_telegram_chat_id=database_message_document["from_telegram_chat_id"],
+            from_room_token=database_message_document["from_room_token"],
+            telegram_message_id=database_message_document["telegram_message_id"],
+        )
+        database_message.refresh()
+        yield database_message
+    else:
+        logger.error("No original messages with the provided ID was found!")
+        yield from list()
