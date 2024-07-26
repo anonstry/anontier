0f77f8cce47cb6636ad4542608604d05a5affefa
Anonstry <anonstry@protonmail.com>
2024-07-26 04:10:48 -0300

    moving the mimic module to a more explicit place

--- src/modules/connection/replicate_action.py
+++ src/modules/connection/replicate_action.py
@@ -7,29 +7,31 @@ from hydrogram.errors.exceptions.bad_request_400 import (
     MessageNotModified,
 )
 from hydrogram.types import Message
-from loguru import logger
+# from loguru import logger
 
-from src import client, methods, session
-
-
-def implement():
-    "Lazy function"
-    logger.info("Module to mimic messages alterations was implemented")
+from src import client
+from src.session.message import Message as DatabaseMessage
+from src.session.message import (
+    search_for_deleted_message_with_id,
+    search_linked_messages,
+)
+from src.session.user import User as DatabaseUser
+from src.telegram.methods import mount_input_media
 
 
 @client.on_edited_message(filters.private & filters.text)
 async def edit_linked_message_text(client: Client, message: Message):
-    user = session.User(message.from_user.id)
+    user = DatabaseUser(message.from_user.id)
     user.create()
     user.refresh()
-    _message = session.Message(
+    _message = DatabaseMessage(
         from_telegram_chat_id=message.chat.id,
         from_room_token=user.room_token,
         telegram_message_id=message.id,
     )
     _message.create()
     _message.refresh()
-    _linked_messages = session.search_linked_messages(_message.token)
+    _linked_messages = search_linked_messages(_message.token)
     for _linked_message in _linked_messages:
         await client.edit_message_text(
             _linked_message.from_telegram_chat_id,
@@ -41,19 +43,19 @@ async def edit_linked_message_text(client: Client, message: Message):
 @client.on_edited_message(filters.private & filters.media)
 async def edit_linked_message_media(client: Client, message: Message):
     print("Message media")
-    user = session.User(message.from_user.id)
+    user = DatabaseUser(message.from_user.id)
     user.create()
     user.refresh()
-    _message = session.Message(
+    _message = DatabaseMessage(
         from_telegram_chat_id=message.chat.id,
         from_room_token=user.room_token,
         telegram_message_id=message.id,
     )
     _message.create()
     _message.refresh()
-    _linked_messages = session.search_linked_messages(_message.token)
+    _linked_messages = search_linked_messages(_message.token)
     message_media = getattr(message, message.media.value)
-    input_media = await methods.mount_input_media(client, message, message_media)
+    input_media = await mount_input_media(client, message, message_media)
     if not input_media:
         return
     for _linked_message in _linked_messages:
@@ -76,12 +78,12 @@ async def delete_linked_messages(client: Client, messages: list[Message]):
     for deleted_message in messages:
         message_id = deleted_message.id
         try:
-            _deleted_message = session.search_for_deleted_message_with_id(message_id)
+            _deleted_message = search_for_deleted_message_with_id(message_id)
         except AssertionError:  # No linked message found to delete
             return
         _deleted_message.create()
         _deleted_message.refresh()
-        _linked_messages = session.search_linked_messages(_deleted_message.token)
+        _linked_messages = search_linked_messages(_deleted_message.token)
         for _linked_message in _linked_messages:
             with suppress(MessageIdInvalid):  # Instead of "Except"
                 await client.delete_messages(
