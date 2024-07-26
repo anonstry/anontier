83fb1b1ea55a2d916c589b36d13bd53d38ece5f9
Anonstry <anonstry@protonmail.com>
2024-07-26 05:03:19 -0300

    improving variables names and fixing imports

--- src/modules/connection/replicate_action.py
+++ src/modules/connection/replicate_action.py
@@ -7,12 +7,11 @@ from hydrogram.errors.exceptions.bad_request_400 import (
     MessageNotModified,
 )
 from hydrogram.types import Message
-# from loguru import logger
 
 from src import client
 from src.session.message import DatabaseMessage
 from src.session.message import (
-    search_for_deleted_message_with_id,
+    search_for_original_messages_with_id,
     search_linked_messages,
 )
 from src.session.user import DatabaseUser
@@ -24,18 +23,18 @@ async def edit_linked_message_text(client: Client, message: Message):
     user = DatabaseUser(message.from_user.id)
     user.create()
     user.refresh()
-    _message = DatabaseMessage(
+    database_message = DatabaseMessage(
         from_telegram_chat_id=message.chat.id,
         from_room_token=user.room_token,
         telegram_message_id=message.id,
     )
-    _message.create()
-    _message.refresh()
-    _linked_messages = search_linked_messages(_message.token)
-    for _linked_message in _linked_messages:
+    database_message.create()
+    database_message.refresh()
+    database_linked_messages = search_linked_messages(database_message.token)
+    for database_linked_message in database_linked_messages:
         await client.edit_message_text(
-            _linked_message.from_telegram_chat_id,
-            _linked_message.telegram_message_id,
+            database_linked_message.from_telegram_chat_id,
+            database_linked_message.telegram_message_id,
             message.text,
         )
 
@@ -46,47 +45,42 @@ async def edit_linked_message_media(client: Client, message: Message):
     user = DatabaseUser(message.from_user.id)
     user.create()
     user.refresh()
-    _message = DatabaseMessage(
+    database_message = DatabaseMessage(
         from_telegram_chat_id=message.chat.id,
         from_room_token=user.room_token,
         telegram_message_id=message.id,
     )
-    _message.create()
-    _message.refresh()
-    _linked_messages = search_linked_messages(_message.token)
+    database_message.create()
+    database_message.refresh()
+    database_linked_messages = search_linked_messages(database_message.token)
     message_media = getattr(message, message.media.value)
     input_media = await mount_input_media(client, message, message_media)
-    if not input_media:
-        return
-    for _linked_message in _linked_messages:
+    assert input_media
+    for database_linked_message in database_linked_messages:
         try:
             await client.edit_message_media(
-                _linked_message.from_telegram_chat_id,
-                _linked_message.telegram_message_id,
+                database_linked_message.from_telegram_chat_id,
+                database_linked_message.telegram_message_id,
                 input_media,
             )
-        except MessageNotModified:  # The media did not change
+        except MessageNotModified:  # The media still the same
             await client.edit_message_caption(
-                _linked_message.from_telegram_chat_id,
-                _linked_message.telegram_message_id,
+                database_linked_message.from_telegram_chat_id,
+                database_linked_message.telegram_message_id,
                 message.caption or str(),
-            )
+            )  # Try to edit or delete the caption
 
 
 @client.on_deleted_messages()
 async def delete_linked_messages(client: Client, messages: list[Message]):
     for deleted_message in messages:
-        message_id = deleted_message.id
-        try:
-            _deleted_message = search_for_deleted_message_with_id(message_id)
-        except AssertionError:  # No linked message found to delete
-            return
-        _deleted_message.create()
-        _deleted_message.refresh()
-        _linked_messages = search_linked_messages(_deleted_message.token)
-        for _linked_message in _linked_messages:
-            with suppress(MessageIdInvalid):  # Instead of "Except"
-                await client.delete_messages(
-                    _linked_message.from_telegram_chat_id,
-                    _linked_message.telegram_message_id,
-                )
+        for database_message in search_for_original_messages_with_id(
+            deleted_message.id
+        ):
+            database_linked_messages = search_linked_messages(database_message.token)
+            for database_linked_message in database_linked_messages:
+                with suppress(MessageIdInvalid):  # Instead of "Except"
+                    await client.delete_messages(
+                        database_linked_message.from_telegram_chat_id,
+                        database_linked_message.telegram_message_id,
+                    )
