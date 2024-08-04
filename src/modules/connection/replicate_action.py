d8b13ec5b3dac286b3583654dd596ea292fd90d6
Anonstry <anonstry@protonmail.com>
2024-08-04 01:57:02 -0300

    hotfix: acessando o atributo do jeito correto

--- src/modules/connection/replicate_action.py
+++ src/modules/connection/replicate_action.py
@@ -83,5 +83,5 @@ async def delete_linked_messages(client: Client, messages: list[Message]):
                         database_linked_message.from_telegram_chat_id,
                         database_linked_message.telegram_message_id,
                     )
-                    if linked_message.from_user.username == client.username:
+                    if linked_message.from_user.username == client.me.username:
                         await linked_message.deleted()
