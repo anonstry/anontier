97ec56a141afd01f275434178570fb2e0d88dca5
Anonstry <anonstry@protonmail.com>
2024-08-04 02:13:22 -0300

    refatorando a ordem das linhas

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,24 @@
 from hydrogram import idle
+from loguru import logger
 
-from src import client
+from src import client, scheduler
+from src.sanitization import schedule_sanization
 
 
 async def routine(app):
     await app.start()
+    logger.info("Bot Telegram client started")
+
+    schedule_sanization(client)
+    logger.info("Scheduled sanitization tasks")
+
+    scheduler.start()
+    logger.info("Scheduler worker started")
+
     await idle()
+
     await app.stop()
+    logger.info("Bot Telegram client stopped")
 
 
 client.run(routine(app=client))
