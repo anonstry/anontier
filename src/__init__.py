d03b25a3b1c163ab7cddc3ee6373a61bff138543
Anonstry <anonstry@protonmail.com>
2024-08-04 04:22:37 -0300

    melhorando o /status

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,11 @@ client: Client = Client(
 )
 
 git_repository = Repo(search_parent_directories=True)
-# git_repository_remote_link = git_repository.remotes[0].config_reader.get("url")
+git_repository_remote_link = (
+    git_repository.remotes[0]
+    .config_reader.get("url")
+    .replace("git@github.com:", "https://github.com/")
+)
 latest_git_repository_tag = sorted(
     git_repository.tags, key=lambda tag: tag.commit.committed_datetime
 )[-1].name
--- src/modules/information.py
+++ src/modules/information.py
@@ -6,7 +6,8 @@ from hydrogram.types import Message
 
 from src.session.user import DatabaseUser
 from src.session.room import Room
-from src import latest_git_repository_commit_shorted
+from src import latest_git_repository_commit_shorted, git_repository_remote_link
+
 
 @Client.on_message(filters.private & filters.command("status"))
 async def show_bot_status(client: Client, message: Message):
@@ -19,6 +20,7 @@ async def show_bot_status(client: Client, message: Message):
         room.refresh()
         caption = file.read_text().format(
             INSTANCE_USERNAME=client.me.username,
+            REPOSITORY_REMOTE_LINK=git_repository_remote_link,
             LAST_COMMIT=latest_git_repository_commit_shorted,
             ROOM_TOKEN=room.token,
             ROOM_PARTICIPANTS_COUNT=room.participants_count,
@@ -27,12 +29,13 @@ async def show_bot_status(client: Client, message: Message):
     except AssertionError:
         caption = file.read_text().format(
             INSTANCE_USERNAME=client.me.username,
+            REPOSITORY_REMOTE_LINK=git_repository_remote_link,
             LAST_COMMIT=latest_git_repository_commit_shorted,
             ROOM_TOKEN="Null",
             ROOM_PARTICIPANTS_COUNT="Null",
             ROOM_SIZE_LIMIT="Null",
         )
-    await message.reply(text=caption, quote=True)
+    await message.reply(text=caption, quote=True, disable_web_page_preview=True)
     message.stop_propagation()
 
 
